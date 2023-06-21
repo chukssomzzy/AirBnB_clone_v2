5566e8ffc53ae5586e96986a32357cc4cae1a310
chukssomzzy <chukssomzzy19@gmail.com>
2023-06-21 21:53:19 +0000

    sql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,13 @@
+-- prepares a MySQL server for the project  
+
+-- CREATE THE DATABASE 
+CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
+
+-- create a new user 
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- grant all privileges on `hbnb_test_db`
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+
+-- grant select privilege on performance_schema 
+GRANT SELECT ON `performance_schema` TO 'hbnb_test'@'localhost';
