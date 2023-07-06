741307145dcc991ed36c05978072ea2e97fe2154
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-06 22:50:53 +0100

    fill

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ DIR_TEST="$DIR_DATA/web_static/releases/test"
 DIR_SHARED="$DIR_DATA/web_static/shared"
 DIR_CUR="$DIR_DATA/web_static/current/"
 USER_CONF="ubuntu"
-NGINX_CONF="/etc/nginx/sites-available/"
+NGINX_CONF="/etc/nginx/sites-available"
 NGINX_ENABLED="/etc/nginx/sites-enabled"
 
 # upgrase the system
@@ -20,11 +20,9 @@ mkdir -p $DIR_TEST
 # mkdir for shared 
 mkdir -p $DIR_SHARED
 
-mkdir -p $DIR_CUR
-
 # create fake html file
 if ! [[ -s "$DIR_TEST/index.html" ]]; then
-cat << EOT > "$DIR_CUR/index.html"
+cat << EOT > "$DIR_TEST/index.html"
 <html>
   <head>
   </head>
@@ -36,7 +34,7 @@ EOT
 fi
 
 # symbolic link  
-ln -s -f $DIR_TEST $DIR_TEST
+ln -s -f $DIR_TEST $DIR_CUR
 
 # chown 
 chown -R $USER_CONF:$USER_CONF $DIR_DATA
@@ -74,12 +72,12 @@ server {
 EOT
 
 if ! [[ -h "$NGINX_ENABLED/default" ]]; then
-ln -s "$NGINX_CONF/default" "$NGINX_ENABLED"
+    ln -s "$NGINX_CONF/default" "$NGINX_ENABLED"
 fi
 
 
 if  nginx -t; then
-service nginx restart 
-exit 0
+    service nginx restart 
+    exit 0
 fi 
 exit 1
