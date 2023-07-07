344d2ccea5450a63f6d783dcfe62ef5e2502255a
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-07 05:26:28 +0000

    do pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,14 +2,13 @@
 """ Archive web_static """
 
 from fabric import task
-from fabric.api import local
 from datetime import datetime
 
 
 @task
-def do_pack():
+def do_pack(c):
     """Run tar on localmachine"""
     cur_date = datetime.now()
     archive_name = cur_date.strftime('web_static_%Y%m%d%H%M%S')
-    local("mkdir versions")
-    local(f"tar -cvsf versions/{archive_name} web_static")
+    c.local("mkdir versions")
+    c.local(f"tar -cvsf versions/{archive_name} web_static")
