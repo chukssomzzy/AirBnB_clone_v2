1c75ed12c5f937fb0e0312d2099af12eca051093
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-07 05:42:52 +0000

    fabfile

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """ Archive web_static """
 
-from fabric.api import local
+from fabric import task
 from datetime import datetime
 import os
 
 
-def do_pack():
+@task
+def do_pack(c):
     """Run tar on localmachine"""
     cur_date = datetime.now()
     archive_name = cur_date.strftime('web_static_%Y%m%d%H%M%S')
     dir_name = 'version'
     if not os.path.isdir(dir_name):
-        if local("mkdir versions").failed:
+        if c.local("mkdir versions").failed:
             return None
-    if not local(f"tar -cvsf versions/{archive_name} web_static"):
+    if not c.local(f"tar -cvsf versions/{archive_name} web_static"):
         return None
     return archive_name
