a6638a3c3dc0fc411fade977b82c03a5bb91dbb6
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-07 05:38:09 +0000

    wrong ime

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """ Archive web_static """
 
-from fabric import task
+from fabric.api import local
 from datetime import datetime
+import os
 
 
-@task
-def do_pack(c):
+def do_pack():
     """Run tar on localmachine"""
     cur_date = datetime.now()
     archive_name = cur_date.strftime('web_static_%Y%m%d%H%M%S')
-    c.local("mkdir versions")
-    c.local(f"tar -cvsf versions/{archive_name} web_static")
+    dir_name = 'version'
+    if not os.path.isdir(dir_name):
+        if local("mkdir versions").failed:
+            return None
+    if not local(f"tar -cvsf versions/{archive_name} web_static"):
+        return None
+    return archive_name
