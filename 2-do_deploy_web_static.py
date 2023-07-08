d1da13d8e891a9b2b236e172860587130e6ddd5e
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-08 22:10:51 +0000

    format

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,12 +11,15 @@ env.hosts = ["54.160.79.245", '18.234.129.129']
 
 def do_deploy(archive_path):
     """Push local archieve to remote server """
-    localfile = './versions/web_static_20230708143924.tgz'
-    remo_releases = '/data/web_static/releases/web_static_20230708143924/'
-    tmp_arch = '/tmp/web_static_20230708143924.tgz'
-    repo_cur = '/data/web_static/current'
+
     if not archive_path or not os.path.isfile(archive_path):
         return False
+    archive_file = archive_path.split('/')[-1]
+    archive_file_noext = archive_file.split('.')[0]
+    localfile = './versions/{}'.format(archive_file)
+    remo_releases = '/data/web_static/releases/{}/'.format(archive_file_noext)
+    tmp_arch = '/tmp/{}'.format(archive_file)
+    repo_cur = '/data/web_static/current'
     try:
         put("{}".format(localfile), "/tmp/")
         run("mkdir -p {}".format(remo_releases))
