ab5cb5f5d4601eb692c64585be518bbf2a93c52e
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-09 14:59:24 +0000

    wrong commit -m

---
+++ 100-clean_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Cleanup outofdate archive file"""
+from fabric.api import local, lcd, env, cd, run
+import os
+
+env.hosts = ["web-01.somzzy.tech", "web-02.somzzy.tech"]
+
+
+def do_cleanup(number=0):
+    """Cleanup archive file"""
+    number = int(number)
+    number = (number == 0 or number == 1) and 1 or number
+    archive_path = "versions/"
+    archive_sorted = sorted(os.listdir(archive_path), reverse=True)
+
+    with lcd("versions/"):
+        for file in archive_sorted[:-number]:
+            local("rm -rf {}".format(file))
+    with cd("/data/web_static/releases"):
+        dirList = run("ls -rt").split()
+        for file in dirList[:-number]:
+            if file.startswith("web_static_"):
+                run("rm -rf {}".format(file))
