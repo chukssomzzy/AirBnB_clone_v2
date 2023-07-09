e8286e780062fbc9692864d2a9d3285c4e214d61
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-09 05:53:26 +0000

    full deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,14 +1,60 @@
 #!/usr/bin/python3
 
 """Full deploy of webstatic"""
+from datetime import datetime
+import os.path
+from fabric.api import env, run, put, local
+from os.path import exists
+env.hosts = ["web-02.somzzy.tech", 'web-01.somzzy.tech']
+
+
+def do_pack():
+    """Run tar on localmachine"""
+    cur_date = datetime.utcnow()
+    date_tar = cur_date.strftime('%Y%m%d%H%M%S')
+    dir_name = 'versions'
+    filepath = "{}/web_static_{}.tgz".format(dir_name, date_tar)
+    try:
+        if not os.path.isdir(dir_name):
+            local("mkdir versions")
+        local("tar -cvzf {} web_static".format(filepath))
+        local("chmod 554 {}".format(filepath))
+        return filepath
+    except Exception:
+        return None
+
+
+def do_deploy(archive_path):
+    """Push local archieve to remote server """
+
+    archive_file = archive_path.split('/')[-1]
+    archive_file_noext = archive_file.split('.')[0]
+    remo_releases = '/data/web_static/releases/{}/'.format(archive_file_noext)
+    tmp_arch = '/tmp/{}'.format(archive_file)
+    repo_cur = '/data/web_static/current'
+    if not archive_path or not exists(archive_path.split('/')[0]):
+        return False
+    try:
+        put("{}".format(archive_path), "/tmp/")
+        run("mkdir -p {}".format(remo_releases))
+        run("tar -xzf {} -C {}".format(tmp_arch, remo_releases))
+        run("rm {}".format(tmp_arch))
+        run("mv {}web_static/* {}".format(remo_releases, remo_releases))
+        run("rm -rf {}web_static".format(remo_releases))
+        run("rm -rf {}".format(repo_cur))
+        run("ln -s {} {}".format(remo_releases, repo_cur))
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
+
+
+archive_path = do_pack()
 
 
 def deploy():
     """Full deploy of webstatic"""
-    do_deploy = __import__("./2-do_deploy_web_static.py").do_pack
-    do_pack = __import__("./1-pack_web_static.py").do_pack
 
-    archive_path = do_pack()
     if archive_path:
         if do_deploy(archive_path=archive_path):
             return True
