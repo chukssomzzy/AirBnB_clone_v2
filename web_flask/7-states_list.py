72449457286da85df7121e3f04be40a4ec8d96d4
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-22 23:33:49 +0100

    commit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,5 +15,11 @@ def state_list():
     return render_template('7-states_list.html', states=states)
 
 
+@app.teardown_appcontext
+def teardown(fnc):
+    """Remove the session"""
+    storage.close()
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
