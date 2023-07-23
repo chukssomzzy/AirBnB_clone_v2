38beff9cf8a2a17cf12c42ca8b55bc69539bb7b7
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-23 11:54:15 +0000

    temp

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Application Server"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template("10-hbnb_filters.html", states=states,
+                           amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown(f):
+    """Close current session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
